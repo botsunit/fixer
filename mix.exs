6f584d693fcdd5a011e28c22b597353327722866
glejeune <gregoire.lejeune@free.fr>
2016-11-20 19:33:51 +0100

    Bump to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fixer.Mixfile do
   def project do
     [
       app: :fixer,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
